436d4080cbbea8b9394b7bc575fdd876e2ced262
webobscure <sparkygino@gmail.com>
2023-05-27 14:03:46 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.css'
+import Header from './components/Header'
+import Chats from './components/Chats'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Chats />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Chats.jsx
@@ -0,0 +1,57 @@
+import react from '../assets/react.svg'
+
+const Chats = () => {
+  return (
+    <>
+      <div className="chats">
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+        <div className="chat-item">
+            <img src={react} alt="" />
+            <p>John Malkovich</p>
+            <p> Choosing a sticker </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chats;
